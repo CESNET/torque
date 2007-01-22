dcec3af1788c887adeb9b72f81133e66569902c8
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2007-01-22 00:26:41 +0000

    mom service port env, backported from trunk r1164
    git-svn-id: svn://clusterresources.com/torque/branches/2.1-fixes@1190 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/server/pbsd_main.c
+++ src/server/pbsd_main.c
@@ -172,7 +172,7 @@ extern char    *msg_daemonname;
 extern int	pbs_errno;
 char	       *pbs_o_host = "PBS_O_HOST";
 pbs_net_t	pbs_mom_addr;
-unsigned int	pbs_mom_port;
+unsigned int	pbs_mom_port = 0;
 unsigned int	pbs_rm_port;
 pbs_net_t	pbs_scheduler_addr;
 unsigned int	pbs_scheduler_port;
@@ -410,6 +410,7 @@ int main(
   uint	 tryport;
   char	 lockfile[MAXPATHLEN + 1];
   char	*pc;
+  char  *ptr;
   job	*pjob;
   pbs_queue *pque;
   char	*servicename = NULL;
@@ -420,9 +421,9 @@ int main(
   time_t last_jobstat_time;
   int    when;
 
-  void	 ping_nodes A_((struct work_task *ptask));
-  void   check_nodes A_((struct work_task *ptask));
-  void   check_log A_((struct work_task *ptask));
+  void ping_nodes A_((struct work_task *));
+  void check_nodes A_((struct work_task *));
+  void check_log A_((struct work_task *));
 
   static struct {
     char *it_name;
@@ -483,8 +484,20 @@ int main(
   pbs_scheduler_port = get_svrport(PBS_SCHEDULER_SERVICE_NAME,"tcp",
     PBS_SCHEDULER_SERVICE_PORT);
 
-  pbs_mom_port = get_svrport(PBS_MOM_SERVICE_NAME,"tcp",
-    PBS_MOM_SERVICE_PORT);
+  ptr = getenv("PBS_MOM_SERVICE_PORT");
+
+  if (ptr != NULL)
+    {
+    pbs_mom_port = (int)strtol(ptr,NULL,10);
+    }
+
+  if (pbs_mom_port <= 0)
+    { 
+    pbs_mom_port = get_svrport(
+      PBS_MOM_SERVICE_NAME,
+      "tcp",
+      PBS_MOM_SERVICE_PORT);
+    }
 
   pbs_rm_port = get_svrport(PBS_MANAGER_SERVICE_NAME,"tcp",
     PBS_MANAGER_SERVICE_PORT);
