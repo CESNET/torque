2203045a611465fbde1086f723d590c2cdeaeaae
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2007-02-17 00:49:26 +0000

    e - allow 'pbsnodes -l' to work with a node spec
    git-svn-id: svn://clusterresources.com/torque/branches/2.1-fixes@1250 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/cmds/pbsnodes.c
+++ src/cmds/pbsnodes.c
@@ -447,14 +447,11 @@ int main(
       }  /* END switch (i) */
     }    /* END while (i == getopt()) */
 
-  if ((errflg != 0) || ((flag == LIST) && (optind != argc))) 
+  if (errflg != 0) 
     {
     if (!quiet)
       {
-      fprintf(stderr,"usage:\t%s [-{c|d|o|p|r}][-s server] [-q] node node ...\n",
-        argv[0]);
-
-      fprintf(stderr,"\t%s -l [-s server] [-q]\n",
+      fprintf(stderr,"usage:\t%s [-{c|d|l|o|p|r}][-s server] [-q] node node ...\n",
         argv[0]);
 
       fprintf(stderr,"\t%s -{a|x} [-s server] [-q] [node]\n",
@@ -483,7 +480,7 @@ int main(
 
   if ((flag == ALLI) || (flag == DOWN) || (flag == LIST) || (flag == DIAG)) 
     {
-    if ((flag == ALLI) || (flag == DIAG))
+    if ((flag == ALLI) || (flag == LIST) || (flag == DIAG))
       {
       /* allow node specification */
 
