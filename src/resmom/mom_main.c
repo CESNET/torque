69c283b2b53185dacfbde4e77deb66a07a57e815
glen <glen@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2007-09-14 13:46:51 +0000

    added Ake's patch
    git-svn-id: svn://clusterresources.com/torque/branches/2.1-fixes@1552 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/resmom/mom_main.c
+++ src/resmom/mom_main.c
@@ -3717,6 +3717,8 @@ int rm_request(
   int   BSpace;
 
   errno = 0;
+  log_buffer[0] = '\0';
+
 
   if (tcp) 
     {
@@ -4470,7 +4472,7 @@ int rm_request(
 
           if ((fp = fopen(config_file,"w+")) == NULL)
             {
-            printf(log_buffer,"cannot open config file %s",
+            sprintf(log_buffer,"cannot open config file %s",
               config_file);
 
             goto bad;
@@ -4480,7 +4482,7 @@ int rm_request(
             {
             fclose(fp);
 
-            printf(log_buffer,"cannot write config file %s",
+            sprintf(log_buffer,"cannot write config file %s",
               config_file);
 
             goto bad;
