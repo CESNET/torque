1669c24c270d3d685cb1d7a0fc74ea3835457093
glen <glen@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2008-07-04 03:51:09 +0000

    fix possible buffer overrun in qsub
    git-svn-id: svn://clusterresources.com/torque/branches/2.3-fixes@2226 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/cmds/qsub.c
+++ src/cmds/qsub.c
@@ -3823,7 +3823,7 @@ int main(
       {
       strcat(submit_args_str,argv[argi]);
 
-      if (argi != optind - 1)
+      if (argi != argc - 1)
         {
         strcat(submit_args_str," ");
         }
