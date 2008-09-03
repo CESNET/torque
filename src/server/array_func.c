a0534c2792790b06b26e5a768a2fc1097e89243c
josh <josh@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2008-09-03 17:14:09 +0000

    FEATURE:	backported code from 2.3-fixes revision 1924 added src/server/array_upgrade.c
    improvements to job_qs_upgrade
    added array_upgrade(), array info now correctly upgraded from 2.2 to 2.3
    (for 2.2 versions without regression that keeps .AR files from being saved!)
    
    ran autoreconf after adding array_upgrade.c in Makefile.am
    
    
    
    
    git-svn-id: svn://clusterresources.com/torque/branches/2.3-yahoo@2366 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/server/array_func.c
+++ src/server/array_func.c
@@ -5,7 +5,7 @@
   is_array() determine if jobnum is actually an array identifyer
   get_array() return array struct for given "parent id"
   array_save() save array struct to disk
-  get_parent_id() return id of parent job if job belongs to a job array
+  array_get_parent_id() return id of parent job if job belongs to a job array
   recover_array_struct() recover the array struct for a job array at restart
   delete_array_struct() free memory used by struct and delete sved struct on disk
  */
@@ -47,6 +47,7 @@
 
 
 extern void  job_clone_wt A_((struct work_task *));
+extern int array_upgrade(job_array *pa, int *fds, int version, int *old_version);
 
 /* global data items used */
 
@@ -174,7 +175,7 @@ int array_save(job_array *pa)
  * and that this will only be called on jobs that actually have or had
  *  a parent job 
  */  
-void get_parent_id(char *job_id, char *parent_id)
+void array_get_parent_id(char *job_id, char *parent_id)
   {
   char *c;
   char *pid;
@@ -214,10 +215,11 @@ job_array *recover_array_struct(char *path)
    job_array *pa;
    array_request_node *rn;
    int fd;
+   int old_version;
    int num_tokens;
    int i;
    
-   
+   old_version = ARRAY_QS_STRUCT_VERSION;
    /* allocate the storage for the struct */
    pa = (job_array*)malloc(sizeof(job_array));
    
@@ -233,10 +235,12 @@ job_array *recover_array_struct(char *path)
 	 
    fd = open(path, O_RDONLY,0);
    
-   /* read the file into the struct previously allocated. 
-    * TODO - check version of struct before trying to read the whole thing
-    */
-   if (read(fd, &(pa->ai_qs), sizeof(pa->ai_qs)) != sizeof(pa->ai_qs))
+   /* read the file into the struct previously allocated. */
+
+   if ((read(fd, &(pa->ai_qs), sizeof(pa->ai_qs)) != sizeof(pa->ai_qs) && 
+        pa->ai_qs.struct_version == ARRAY_QS_STRUCT_VERSION) || 
+       (pa->ai_qs.struct_version != ARRAY_QS_STRUCT_VERSION && 
+        array_upgrade(pa, &fd, pa->ai_qs.struct_version, &old_version) != 0))
      {
      sprintf(log_buffer,"unable to read %s", path);
 
@@ -247,17 +251,11 @@ job_array *recover_array_struct(char *path)
      return NULL;
      }
 
-   if (pa->ai_qs.struct_version != ARRAY_QS_STRUCT_VERSION)
-     {
-     /* TODO */
-     }
-     
-   /* check to see if there is any additional info saved in the array file */
-   
+   /* check to see if there is any additional info saved in the array file */   
    /* check if there are any array request tokens that haven't been fully
    processed */
    
-   if (pa->ai_qs.struct_version > 1 )
+   if (old_version > 1)
      {
      if (read(fd, &num_tokens, sizeof(int)) != sizeof(int))
        {
@@ -273,7 +271,7 @@ job_array *recover_array_struct(char *path)
          {
 	 rn = (array_request_node*)malloc(sizeof(array_request_node));
 	 
-	 if (read(fd, &num_tokens, sizeof(int)) != sizeof(int))
+	 if (read(fd, rn, sizeof(array_request_node)) != sizeof(array_request_node))
            {
            
 	   sprintf(log_buffer, "error reading array_request_node from %s", path);
@@ -295,17 +293,23 @@ job_array *recover_array_struct(char *path)
            }
 	   
 	   CLEAR_LINK(rn->request_tokens_link);
-	   append_link(&pa->request_tokens, &rn->request_tokens_link, (void*)rn);
+	   append_link(&pa->request_tokens,&rn->request_tokens_link,(void*)rn);
 	 
 	 }
        
          
      
      }
+  
    
    
      
    close(fd);
+
+   if (old_version != ARRAY_QS_STRUCT_VERSION)
+     {
+     array_save(pa);
+     }
 	 
    /* link the struct into the servers list of job arrays */ 
    append_link(&svr_jobarrays, &pa->all_arrays, (void*)pa);
