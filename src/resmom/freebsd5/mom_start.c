6385dacaa81b95ea4b5bc3e46102e90acf7fad29
ataufer <ataufer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2009-05-06 21:33:40 +0000

    b - added missing routine so we can build on FreeBSD6 when CACHEOBITFAILURES is defined (related to rev 2500)
    git-svn-id: svn://clusterresources.com/torque/branches/2.3-fixes@2915 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/resmom/freebsd5/mom_start.c
+++ src/resmom/freebsd5/mom_start.c
@@ -113,6 +113,8 @@ extern int  termin_child;
 extern int       LOGLEVEL;
 
 /* Private variables */
+#define TMAX_TJCACHESIZE 128
+job *TJCache[TMAX_TJCACHESIZE];
 
 /*
  * set_job - set up a new job session
@@ -207,8 +209,6 @@ struct passwd *pwdp;
  * task as Exiting.
  */
 
-#define TMAX_TJCACHESIZE 128
-
 void
 scan_for_terminated(void)
   {
@@ -221,8 +221,6 @@ scan_for_terminated(void)
 
 
 #ifdef CACHEOBITFAILURES
-  static job *TJCache[TMAX_TJCACHESIZE];
-
   int tjcindex;
 
   int TJCIndex = 0;
@@ -488,6 +486,38 @@ scan_for_terminated(void)
 
   return;
   }  /* END scan_for_terminated() */
+
+
+
+/**
+ * Removes any cached obits relating to the given job.
+ */
+void remove_job_from_obit_cache(
+
+  job *pjob)  /* I */
+
+  { 
+#ifdef CACHEOBITFAILURES
+  int TJCIndex;
+
+  /* remove any reference to this job in the obit cache */
+
+  for (TJCIndex = 0;TJCIndex < TMAX_TJCACHESIZE;TJCIndex++)
+    {
+    if (TJCache[TJCIndex] == NULL)
+      break;
+
+    if (TJCache[TJCIndex] == (job *)1)
+      continue;
+
+    if (TJCache[TJCIndex] == pjob)
+      TJCache[TJCIndex] = (job *)1;  /* clear slot */
+    }
+#endif /* CACHEOBITFAILURES */
+  }  /* END remove_job_from_obit_cache() */
+
+
+
 
 /*
  * creat the master pty, this particular
