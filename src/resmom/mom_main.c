965071684c970d96f92bb9c64bc6f8523d487fc4
ataufer <ataufer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2010-03-08 19:43:38 +0000

    b - fixed where job was added to obit retry list multiple times, causing a segfault
    git-svn-id: svn://clusterresources.com/torque/branches/2.4-fixes@3488 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/resmom/mom_main.c
+++ src/resmom/mom_main.c
@@ -7856,7 +7856,7 @@ void examine_all_jobs_to_resend(void)
     if (JobsToResend[jindex] == (job *)DUMMY_JOB_PTR)
       continue;
 
-    if (!post_epilogue(JobsToResend[jindex],-5))
+    if (!post_epilogue(JobsToResend[jindex], MOM_OBIT_RETRY))
       {
 
       if (LOGLEVEL >= 7)
