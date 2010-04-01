b95c86bbf3b85a38b1212222a13e98b61f1c76fa
itobler <itobler@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2010-04-01 17:28:00 +0000

    TESTING: updating tests
    git-svn-id: svn://clusterresources.com/torque/branches/2.4-fixes@3548 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- test/t/blcr/install/install.bat
+++ test/t/blcr/install/install.bat
@@ -1,21 +1,17 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
 
 use FindBin;
 use lib "$FindBin::Bin/../../../lib/";
 
-
 use CRI::Test;
-
 plan('no_plan');
 setDesc("Install 'Berkley Lab Checkpoint/Restart (BLCR) for Linux'");
 
 my $testbase = $FindBin::Bin;
 
-
-execute_tests(
+my @setup_steps = (
         "$testbase/010_get_blcr_src.t",
         "$testbase/030_configure_blcr.t",
         "$testbase/040_make_blcr.t",
@@ -23,6 +19,12 @@ execute_tests(
 #        "$testbase/060_make_check_blcr.t",
         "$testbase/070_make_install_blcr.t", 
         );
+
+foreach( @setup_steps )
+{
+  execute_tests($_)
+    or die 'BLCR Setup Failed!';
+}
 
 execute_tests(
 #        "$testbase/environment_config.bat", # This can be inconsistant
