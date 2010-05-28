d4f8726e652c4874e344103a90b61217a2629b0d
Simon Toth <SimonT@mail.muni.cz>
2010-05-28 12:16:51 +0200

    Do not change state when not needed.

--- src/server/req_stat.c
+++ src/server/req_stat.c
@@ -812,7 +812,8 @@ static void stat_update(
       {
       if ((pjob = find_job(pstatus->brp_objname)))
         {
-        if (is_cloud_job(pjob))
+        if (is_cloud_job(pjob) &&
+            pjob->ji_qs.ji_substate == JOB_SUBSTATE_PRERUN_CLOUD)
           svr_setjobstate(pjob, JOB_STATE_RUNNING, JOB_SUBSTATE_RUNNING);
 
         sattrl = (svrattrl *)GET_NEXT(pstatus->brp_attr);
