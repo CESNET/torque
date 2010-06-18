05070e78ab30bc3ef96d82791cc5bfd7d0bca000
garrick <garrick@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2010-06-18 18:27:29 +0000

    update merge from trunk@3730
    git-svn-id: svn://clusterresources.com/torque/branches/NUMA@3732 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/server/job_func.c
+++ src/server/job_func.c
@@ -170,6 +170,7 @@ extern int   LOGLEVEL;
 
 extern tlist_head svr_newjobs;
 extern tlist_head svr_alljobs;
+extern tlist_head svr_jobs_array_sum;
 extern char *path_checkpoint;
 
 extern struct batch_request *setup_cpyfiles(struct batch_request *, job *, char *, char *, int, int);
@@ -1856,13 +1857,23 @@ job *find_job(
   char *at;
   char *comp;
   int   different = FALSE;
+  int   array = FALSE;
 
   job  *pj;
 
   if ((at = strchr(jobid, (int)'@')) != NULL)
     * at = '\0'; /* strip off @server_name */
 
-  pj = (job *)GET_NEXT(svr_alljobs);
+  /* if its the name of a job array, look in the other list */
+  if (strstr(jobid,"[]") != NULL)
+    {
+    pj = (job *)GET_NEXT(svr_jobs_array_sum);
+    array = TRUE;
+    }
+  else
+    {
+    pj = (job *)GET_NEXT(svr_alljobs);
+    }
 
   if ((server.sv_attr[SRV_ATR_display_job_server_suffix].at_flags & ATR_VFLAG_SET) ||
       (server.sv_attr[SRV_ATR_job_suffix_alias].at_flags & ATR_VFLAG_SET))
@@ -1883,7 +1894,14 @@ job *find_job(
     if (!strcmp(comp, pj->ji_qs.ji_jobid))
       break;
 
-    pj = (job *)GET_NEXT(pj->ji_alljobs);
+    if (array == TRUE)
+      {
+      pj = (job *)GET_NEXT(pj->ji_jobs_array_sum);
+      }
+    else
+      {
+      pj = (job *)GET_NEXT(pj->ji_alljobs);
+      }
     }
 
   if (at)
