8b40022ce369175e590ca85533c592f53d770adc
Simon Toth <SimonT@mail.muni.cz>
2010-06-28 12:37:47 +0200

    Keep cloud jobs in transit state while building.

--- src/server/svr_jobfunc.c
+++ src/server/svr_jobfunc.c
@@ -2232,6 +2232,10 @@ void set_statechar(
     {
     pjob->ji_wattr[JOB_ATR_state].at_val.at_char = suspend;
     }
+  else if (pjob->ji_qs.ji_substate == JOB_SUBSTATE_PRERUN_CLOUD)
+    {
+    pjob->ji_wattr[JOB_ATTR_state].at_val.at_char = 'T';
+    }
   else
     {
     if (pjob->ji_qs.ji_state < (int)strlen(statechar))
