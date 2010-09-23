05e546a86c157e971e57a66fa34a16d6a4e93c5d
Simon Toth <SimonT@mail.muni.cz>
2010-09-23 14:47:20 +0200

    Assertions fix.

--- src/include/assertions.h
+++ src/include/assertions.h
@@ -7,23 +7,24 @@
 #include "pbs_error.h"
 #include <stdlib.h>
 
-static char whereami[120];
+/* TODO provide better solution, this is ugly */
+static char whereami[1024];
 
 #define dbg_precondition(expr, comment) \
   { if (!(expr)) { \
-  sprintf(whereami,"%s:%d",__func__,__LINE__);\
+  sprintf(whereami,"%s() %s:%d",__func__, __FILE__, __LINE__);\
   sprintf(log_buffer,"%s is false -> \"%s\"",#expr,comment);\
   log_err(PBSE_PRECONDITION,whereami,log_buffer); abort(); } }
 
 #define dbg_postcondition(expr, comment) \
   { if (!(expr)) { \
-  sprintf(whereami,"%s:%d",__func__,__LINE__);\
+  sprintf(whereami,"%s() %s:%d",__func__, __FILE__, __LINE__);\
   sprintf(log_buffer,"%s is false -> \"%s\"",#expr,comment);\
   log_err(PBSE_POSTCONDITION,whereami,log_buffer); abort(); } }
 
 #define dbg_consistency(expr, comment) \
   { if (!(expr)) { \
-  sprintf(whereami,"%s:%d",__func__,__LINE__);\
+  sprintf(whereami,"%s() %s:%d",__func__, __FILE__, __LINE__);\
   sprintf(log_buffer,"%s is false -> \"%s\"",#expr,comment);\
   log_err(PBSE_CONSISTENCY,whereami,log_buffer); abort(); } }
 
