fd40b1de7c1debf5b81bf520a3f93a9e8ce85b4b
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-02-15 18:56:59 +0000

    Cpusets were not being deleted when geometry requests and cpusets were both enabled unless a geometry request was made, but this is fixed now
    git-svn-id: svn://clusterresources.com/torque/branches/3.0-fixes@4483 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/cmds/qsub.c
+++ src/cmds/qsub.c
@@ -2821,7 +2821,6 @@ int validate_group_list(
   char         **pmem;
   struct group  *grent;
   struct passwd *pwent;
-  int            is_member = FALSE;
 
   pwent = getpwuid(getuid());
   u_name = pwent->pw_name;
