38902aa2dccb9c49644770425cc3e483a21ad26c
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-05-26 18:18:15 +0000

    Add some checks to buffer boundaries to reduce vulnerability to buffer overflow attacks
    git-svn-id: svn://clusterresources.com/torque/branches/3.0-fixes@4680 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/server/req_quejob.c
+++ src/server/req_quejob.c
@@ -1277,17 +1277,19 @@ void req_jobscript(
     {
     if (errno == 0)
       {
-      sprintf(log_buffer, "job %s in unexpected state '%s'",
-              pj->ji_qs.ji_jobid,
-              PJobSubState[pj->ji_qs.ji_substate]);
+      snprintf(log_buffer, sizeof(log_buffer),
+        "job %s in unexpected state '%s'",
+        pj->ji_qs.ji_jobid,
+        PJobSubState[pj->ji_qs.ji_substate]);
       }
     else
       {
-      sprintf(log_buffer, "job %s in unexpected state '%s' (errno=%d - %s)",
-              pj->ji_qs.ji_jobid,
-              PJobSubState[pj->ji_qs.ji_substate],
-              errno,
-              strerror(errno));
+      snprintf(log_buffer, sizeof(log_buffer),
+        "job %s in unexpected state '%s' (errno=%d - %s)",
+        pj->ji_qs.ji_jobid,
+        PJobSubState[pj->ji_qs.ji_substate],
+        errno,
+        strerror(errno));
       }
 
     log_err(errno, id, log_buffer);
@@ -1488,9 +1490,10 @@ void req_mvjobfile(  /* NOTE:  routine for server only - mom code follows this r
 
   if (LOGLEVEL >= 6)
     {
-    sprintf(log_buffer, "successfully moved file '%s' for job '%s'",
-            namebuf,
-            preq->rq_ind.rq_jobfile.rq_jobid);
+    snprintf(log_buffer, sizeof(log_buffer),
+      "successfully moved file '%s' for job '%s'",
+      namebuf,
+      preq->rq_ind.rq_jobfile.rq_jobid);
 
     log_record(
       PBSEVENT_JOB,
@@ -1606,9 +1609,10 @@ void req_rdytocommit(
     {
     char tmpLine[1024];
 
-    sprintf(tmpLine, "cannot save job - errno=%d - %s",
-            errno,
-            strerror(errno));
+    snprintf(tmpLine, sizeof(tmpLine),
+      "cannot save job - errno=%d - %s",
+      errno,
+      strerror(errno));
 
     log_err(errno, id, tmpLine);
 
@@ -1632,9 +1636,10 @@ void req_rdytocommit(
     {
     /* reply failed, purge the job and close the connection */
 
-    sprintf(log_buffer, "cannot report jobid - errno=%d - %s",
-            errno,
-            strerror(errno));
+    snprintf(log_buffer, sizeof(log_buffer),
+      "cannot report jobid - errno=%d - %s",
+      errno,
+      strerror(errno));
 
     log_err(errno, id, log_buffer);
 
@@ -1924,11 +1929,12 @@ void req_commit(
 
   /* need to format message first, before request goes away */
 
-  sprintf(log_buffer, msg_jobnew,
-          preq->rq_user, preq->rq_host,
-          pj->ji_wattr[JOB_ATR_job_owner].at_val.at_str,
-          pj->ji_wattr[JOB_ATR_jobname].at_val.at_str,
-          pj->ji_qhdr->qu_qs.qu_name);
+  snprintf(log_buffer, sizeof(log_buffer),
+    msg_jobnew,
+    preq->rq_user, preq->rq_host,
+    pj->ji_wattr[JOB_ATR_job_owner].at_val.at_str,
+    pj->ji_wattr[JOB_ATR_jobname].at_val.at_str,
+    pj->ji_qhdr->qu_qs.qu_name);
 
   /* acknowledge the request with the job id */
 
@@ -1963,8 +1969,10 @@ void req_commit(
     {
     if (LOGLEVEL >= 7)
       {
-      sprintf(log_buffer, "Trying to AUTORUN job %s",
-              pj->ji_qs.ji_jobid);
+      snprintf(log_buffer, sizeof(log_buffer),
+        "Trying to AUTORUN job %s",
+        pj->ji_qs.ji_jobid);
+
       log_record(
         PBSEVENT_JOB,
         PBS_EVENTCLASS_JOB,
