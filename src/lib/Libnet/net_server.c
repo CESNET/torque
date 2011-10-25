5c8b66ad94a3774d324cdce000b86ad8422c0813
knielson <knielson@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-10-25 21:34:18 +0000

    Merged revision 5258, added DIS_tcp_close to code base. Merged revision 5242, changed getpwnam_ext to getpwnam in pam_pbssimpleauth.c
    git-svn-id: svn://clusterresources.com/torque/branches/3.0-fixes@5259 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/lib/Libnet/net_server.c
+++ src/lib/Libnet/net_server.c
@@ -114,6 +114,7 @@
 #include "server_limits.h"
 #include "net_connect.h"
 #include "log.h"
+#include "dis.h" /* DIS_tcp_close */
 
 extern int  LOGLEVEL;
 
@@ -717,6 +718,7 @@ void close_conn(
   int sd) /* I */
 
   {
+
   if ((sd < 0) || (max_connection <= sd))
     {
     return;
@@ -755,6 +757,9 @@ void close_conn(
   svr_conn[sd].cn_authen = 0;
 
   num_connections--;
+
+  DIS_tcp_close(sd);
+
 
   return;
   }  /* END close_conn() */
