b8ff25540a3f8a269019e1fdc454332d428d7ce1
knielson <knielson@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-11-03 16:22:48 +0000

    merged revision 5324 from 2.5-fixes. Make it possible to set procct without setting nodes
    git-svn-id: svn://clusterresources.com/torque/branches/3.0-fixes@5330 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/server/req_quejob.c
+++ src/server/req_quejob.c
@@ -233,10 +233,13 @@ int set_node_attr(job *pjob)
 
     if(nodect_set == 0)
       {
-      /* neither procs nor nodes were requested. set nodes to 1
-         so the procct will process correctly. */
-      rc = decode_resc(&pjob->ji_wattr[JOB_ATR_resource], ATTR_l, "nodes", "1");
-     }
+      int temp_perm;
+      /* neither procs nor nodes were requested. set procct to 1 */
+      temp_perm = resc_access_perm;
+      resc_access_perm = ATR_DFLAG_WRACC | ATR_DFLAG_MGWR | ATR_DFLAG_RMOMIG;
+      rc = decode_resc(&pjob->ji_wattr[JOB_ATR_resource], "Resource_List", "procct", "1");
+      resc_access_perm = temp_perm;
+      }
 
   return(rc);
   }  
