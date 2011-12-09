0e13b2ecb35eb9796dd6fd249843cb639c720986
knielson <knielson@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2011-12-09 16:58:10 +0000

    merged revision 5471 from 2.5-fixes. Fixed a problem with interactive jobs exiting prematurely.
    git-svn-id: svn://clusterresources.com/torque/branches/3.0-fixes@5472 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/server/job_route.c
+++ src/server/job_route.c
@@ -204,7 +204,7 @@ badplace *is_bad_dest(
  * Returns 0 on success. Non-zero on failure
  */ 
 int initialize_procct(job *pjob)
-  {                \
+  { 
   char id[] = "initialize_procct";
   resource     *pnodesp = NULL;
   resource_def *pnodes_def = NULL;
@@ -369,7 +369,7 @@ int remove_procct(job *pjob)
       pctdef->rs_free(&pctresc->rs_value);
 
   return(PBSE_NONE);
-  }
+  } /* END remove_procct */
 
 /*
  * default_router - basic function for "routing" jobs.
