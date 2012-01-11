8188b68959f2c2f8738cde150dc1327c362a8867
dbeer <dbeer@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2012-01-11 22:50:48 +0000

    Fix a warning that shouldn't have been in the last patch
    git-svn-id: svn://clusterresources.com/torque/branches/3.0-fixes@5615 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/resmom/mom_main.c
+++ src/resmom/mom_main.c
@@ -4408,7 +4408,6 @@ process_hup(void)
 
   {
   char *id = "process_hup";
-  char  old_servernames[PBS_MAXSERVER][PBS_MAXSERVERNAME + 1];
 
   call_hup = 0;
   log_record(PBSEVENT_SYSTEM, 0, id, "reset");
