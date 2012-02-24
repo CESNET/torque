c3871b72b6f27448b06d744aaf759c12b34cde92
jrosenquist <jrosenquist@8f940c70-5916-0410-ac46-d1fa2fa6ea29>
2012-02-24 15:53:52 +0000

    Fixed bugs TRQ-777, TRQ-762, and another ticket that doesn't have a jira yet.
    git-svn-id: svn://clusterresources.com/torque/branches/3.0-fixes@5792 8f940c70-5916-0410-ac46-d1fa2fa6ea29

--- src/server/array_func.c
+++ src/server/array_func.c
@@ -1465,6 +1465,7 @@ void update_array_statuses()
   {
   job_array *pa;
   job *pj;
+  char tmp_buf[1025];
   int i;
   unsigned int running;
   unsigned int queued;
@@ -1504,8 +1505,15 @@ void update_array_statuses()
           }
         }
       }
-    
-    if (running > 0)
+    if (pa->template_job == NULL)
+      {
+      /* Skip this item. */
+      snprintf(tmp_buf, 1024,
+          "Error in job array [%s]. It appears that there are no jobs left for this array and the .AR file (array) associated with the job may need to be deleted.\n",
+          pa->ai_qs.parent_id);
+      log_err(-1, "update_array_statuses", tmp_buf);
+      }
+    else if (running > 0)
       {
       svr_setjobstate(pa->template_job, JOB_STATE_RUNNING, pa->template_job->ji_qs.ji_substate);
       }
