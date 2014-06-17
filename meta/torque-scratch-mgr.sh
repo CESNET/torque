6221048859b043fb60277e2d1f69281f8b7bfc99
Mgr. Simon Toth <simon@cesnet.cz>
2014-06-17 14:41:13 +0200

    Tweaks in scratch handling

--- meta/torque-scratch-mgr.sh
+++ meta/torque-scratch-mgr.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 function dead_quota_size {
-	du -sk $1 2>/dev/null | sed 's/\t/ /' | awk '{ print $1 }';
+	du -sk "$1" 2>/dev/null | sed 's/\t/ /' | awk '{ print $1 }';
+}
+
+function dead_quota_sum {
+	if [ -d "$1" ]; then
+		find "$1/" -maxdepth 2 -type f -name "*.size" -exec cat {} \; | awk '
+		BEGIN { sum=0; } { sum+=$1; } END { print sum; }'
+	else
+		echo 0
+	fi
 }
