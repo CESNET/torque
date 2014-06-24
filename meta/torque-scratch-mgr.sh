c1c1d5d202a032570d1c0d7e04258e6e13f029e0
Mgr. Simon Toth <simon@cesnet.cz>
2014-06-24 12:21:12 +0200

    Only report dead scratch per-user

--- meta/torque-scratch-mgr.sh
+++ meta/torque-scratch-mgr.sh
@@ -12,3 +12,12 @@ function dead_quota_sum {
 		echo 0
 	fi
 }
+
+function dead_quota_user_sum {
+	if [ -d "$1" ]; then
+		find "$1/" -maxdepth 1 -type f -name "*.size" -exec cat {} \; | awk '
+		BEGIN { sum=0; } { sum+=$1; } END { print sum; }'
+	else
+		echo 0
+	fi
+}
