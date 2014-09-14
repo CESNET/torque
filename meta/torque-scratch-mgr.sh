53272c3b64ac83538c16812b616c0c76ba2fe450
Mgr. Simon Toth <simon@cesnet.cz>
2014-09-14 23:47:18 +0200

    Scratch scripts improvements

--- meta/torque-scratch-mgr.sh
+++ meta/torque-scratch-mgr.sh
@@ -6,7 +6,7 @@ function dead_quota_size {
 
 function dead_quota_sum {
 	if [ -d "$1" ]; then
-		find "$1/" -maxdepth 2 -type f -name "*.size" -exec cat {} \; | awk '
+		find "$1/" -mindepth 2 -maxdepth 2 -type f -name "*.size" -exec cat {} \; | awk '
 		BEGIN { sum=0; } { sum+=$1; } END { print sum; }'
 	else
 		echo 0
@@ -15,7 +15,7 @@ function dead_quota_sum {
 
 function dead_quota_user_sum {
 	if [ -d "$1" ]; then
-		find "$1/" -maxdepth 1 -type f -name "*.size" -exec cat {} \; | awk '
+		find "$1/" -mindepth 1 -maxdepth 1 -type f -name "*.size" -exec cat {} \; | awk '
 		BEGIN { sum=0; } { sum+=$1; } END { print sum; }'
 	else
 		echo 0
